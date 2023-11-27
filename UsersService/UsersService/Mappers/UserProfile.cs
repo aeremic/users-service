f23e42cbd5177295b5c50bec78b6c0150cb7add5
Andrija Eremic <andrija.eremic@gmail.com>
2023-11-27 22:43:39 +0100

    mapper fix

---
+++ UsersService/UsersService/Mappers/UserProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using UsersService.Domain;
+using UsersService.Queries.User.GetUserData;
+
+namespace UsersService.Mappers;
+
+public class UserProfile : Profile
+{
+    public UserProfile()
+    {
+        CreateMap<User, UserDataDto>();
+    }
+}
